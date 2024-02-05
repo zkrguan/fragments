13050255f412efdff632e7a117d349291eab3ce7
Zhaokai Guan <zguan25@myseneca.ca>
2024-02-05 15:17:30 -0500

    Loggers set

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -6,9 +6,6 @@ const logger = require('../../logger');
  */
 exports.postCreateFragment = async function (req, res) {
     try {
-        logger.info(req);
-        logger.info(req.email);
-        logger.info(req.user);
         const fragment = new Fragment({
             ownerId: req.user,
             type: req.headers['content-type'],
@@ -19,13 +16,10 @@ exports.postCreateFragment = async function (req, res) {
         res.location(`http://${req.headers.host}/v1/fragments/${fragment.id}`);
         res.status(201).json(
             response.createSuccessResponse({
-                status: 'ok',
                 fragment: fragment,
             })
         );
     } catch (err) {
-        logger.error(err);
-
         if (err === 'No matching type') {
             res.status(415).json(
                 response.createErrorResponse(
@@ -34,6 +28,8 @@ exports.postCreateFragment = async function (req, res) {
                 )
             );
         } else {
+            logger.error(`Unexpected error occured in POST fragments/ route`);
+            logger.debug(err);
             res.status(400).json(
                 response.createErrorResponse(
                     400,
