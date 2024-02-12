72a7354db42eaaa7c492f774f3bb3a8abde7bd28
Zhaokai Guan <zguan25@myseneca.ca>
2024-02-11 21:03:05 -0500

    did some final touch up

--- jest.config.js
+++ jest.config.js
@@ -10,8 +10,6 @@ require('dotenv').config({ path: envFile });
 
 // Log a message to remind developers how to see more detail from log messages
 
-console.log(`Using LOG_LEVEL=${process.env.LOG_LEVEL}. Use 'debug' in env.jest for more detail`);
-
 // Set our Jest options, see https://jestjs.io/docs/configuration
 module.exports = {
     verbose: true,
--- test/unit/app.test.js
+++ test/unit/app.test.js
@@ -4,7 +4,6 @@ const app = require('../../src/app');
 describe('/ health check', () => {
     test('should return status error, code 404, message not found', async () => {
         const res = await request(app).get('/iAmNotExisted');
-        console.log(res.body.error);
         expect(res.body.status).toEqual('error');
         expect(res.body.error['message']).toEqual('not found');
         expect(res.body.error['code']).toEqual(404);
